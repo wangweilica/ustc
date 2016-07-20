82201bd14bc5ca34d65e834490ea847aeff3e856
wangwei <wangwei@haochedai.cn>
2016-07-20 18:18:08 +0800

    高级人工智能作业：八数码问题性能分析

--- src/main/java/ustc/AAISearch.java
+++ src/main/java/ustc/AAISearch.java
@@ -3,17 +3,33 @@ package ustc;
 /**
  * Author: Wangwei
  * Date: 2016/7/20
- * Desc:
+ * Desc:八数码问题
  */
 public class AAISearch {
 
-    public static void resolve(int[] source,int[] target) {
+    /**
+     * 启发函数：W(n)不在位的将牌数
+     * @param source 初试状态
+     * @param target 目标状态
+     */
+    public static void resolveWithW(int[] source,int[] target) {
         if (source.length ==  AAIUtil.ARRAY_LEN && target.length == AAIUtil.ARRAY_LEN) {
 
 
         }
     }
 
+    /**
+     * 启发函数：P(n)每一个将牌与其目标位置之间距离的(不考虑夹在其间的将牌)总和
+     * @param source 初试状态
+     * @param target 目标状态
+     */
+    public static void resolveWithP(int[] source,int[] target) {
+        if (source.length ==  AAIUtil.ARRAY_LEN && target.length == AAIUtil.ARRAY_LEN) {
+
+
+        }
+    }
 
 
 
